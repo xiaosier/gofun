17fa11e6231065f2de8cb54dda913f0aea0cc76e
lazypeople <hfutming@gmail.com>
2019-07-21 14:11:51 +0800

    Create token.php
    ADD WEIXIN TOKEN EXAMPLE

---
+++ token.php
@@ -0,0 +1,96 @@
+<?php
+
+
+define("TOKEN", "weixin");
+traceHttp();
+$wechatObj = new wechatCallbackapiTest();
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
+} else {
+    $wechatObj->responseMsg();
+}
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        if ($this->checkSignature()) {
+            header('content-type:text');
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr == $signature) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)) {
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_OCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            <FuncFlag>0</FuncFlag>
+            </xml>";
+            if ($keyword == "?" || $keyword == "？") {
+                $msgType = "text";
+                $content = date("Y-m-d H:i:s", time());
+                $result = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType,
+                    $content);
+                echo $result;
+            }
+        } else {
+            echo "";
+            exit;
+        }
+    }
+}
+
+function traceHttp()
+{
+    logger("\n\nREMOTE_ADDR:" . $_SERVER["REMOTE_ADDR"] . (strstr($_SERVER["REMOTE_ADDR"], '101.226') ? " FROM WeiXin" : "Unknown IP"));
+    logger("QUERY_STRING:" . $_SERVER["QUERY_STRING"]);
+}
+
+function logger($log_content)
+{
+    if (isset($_SERVER['HTTP_APPNAME'])) {   //SAE
+        sae_set_display_errors(false);
+        sae_debug($log_content);
+        sae_set_display_errors(true);
+    } else { //LOCAL
+        $max_size = 500000;
+        $log_filename = "log.xml";
+        if (file_exists($log_filename) and (abs(filesize($log_filename)) > $max_size)) {
+            unlink($log_filename);
+        }
+        file_put_contents($log_filename, date('Y-m-d H:i:s') . $log_content . "\r\n", FILE_APPEND);
+    }
+}
